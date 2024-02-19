972893f1f0443683cd99b3dbac028cc89c01a996
AlexisMcIntyre <desertamethyst@gmail.com>
2024-02-18 18:59:19 -0800

    minor change

--- src/Components/CommentComponent.js
+++ src/Components/CommentComponent.js
@@ -4,12 +4,11 @@ import DislikeButton from './DislikeButton';
 
 export const CommentComponent = (props) => {
     const {comment, deleteComment} = props; //house is all data passed in for house, updateHouse is the method being passed in (called from child class component)
-    const {index, deleteIndex} = props
     
     return (
             <div className="comment">    
                 <p>{comment.comment}</p>
-                <button onClick={(e) => deleteComment(index)}>Delete</button>
+                <button onClick={(e) => deleteComment(props.index)}>Delete</button>
                 <LikeButton /><DislikeButton />
             </div>
     )
